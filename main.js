3b6608fcd182287f9bf5e5b16a0988f734fe5ea7
Veronika Novakova <veronika3novak@gmail.com>
2021-03-16 21:01:54 +0100

    DU initial changes

--- main.js
+++ main.js
@@ -1,1 +1,62 @@
 // tady je místo pro náš program
+
+let nadpis = document.querySelector ("h1");
+nadpis.classList.add("zluty");
+//nadpis.classList.remove("zluty");
+
+function naKlikTucne() {
+    document.querySelector("p").style.fontWeight = "bold";
+}
+
+function priOdchodu() {
+    document.querySelector("p").style.fontWeight = "normal";
+}
+
+function zcervena() {
+    document.querySelector("p").style.color = "red";
+}
+
+function zcerna() {
+    document.querySelector("p").style.color = "black";
+}
+
+function vetsiOPixel() {
+    document.querySelector("p").style.fontSize = "17px";
+}
+
+//Fukce, která zahraje písničku 
+function prehraj() {
+    let audioElement = document.querySelector('#js-audio');
+    audioElement.play();
+}
+
+//Funkce, která písničku zastaví
+function zastav() {
+    let audioElement = document.querySelector('#js-audio');
+    audioElement.pause();
+}
+
+//Funkce, která písničku ztiší
+function ztisit() {
+    let audioElement = document.querySelector('#js-audio');
+    audioElement.volume()=0.1;
+}
+
+//Funkce, která písničku dát na akorát hlasitost
+function stredniHlasitost() {
+    let audioElement = document.querySelector('#js-audio');
+    audioElement.volume()=0.5;
+}
+
+//Funkce, která písničku dát na maximální hlasitost
+function maxHlasitost() {
+    let audioElement = document.querySelector('#js-audio');
+    audioElement.volume()=1;
+}
+
+//Funkce, která písničku vrátí na začátek
+function zpatkyNaZacatek() {
+    let audioElement = document.querySelector('#js-audio');
+    audioElement.loop();
+}
+
